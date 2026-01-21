e856c168ce1c07cf1a917e17013c42160af047c4
Sikhumbuzo Mayapi <skhura96@gmail.com>
2026-01-22 00:56:43 +0200

    added icons on the metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,13 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Circuit Guardians | Tech Repair & Custom Software Solutions",
+  title: "Circuit Guardians | Your Tech Heroes",
   description: "From broken screens to custom apps. We offer mobile & laptop repairs, software installation, IT support, and full web/app development. Your one-stop tech partner in South Africa.",
   keywords: "tech repair, laptop repair, mobile repair, web development, app development, software installation",
   authors: [{ name: "Circuit Guardians" }],
+  icons: {
+    icon: '/logo.png',
+  },
 };
 
 export default function RootLayout({
